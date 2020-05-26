ac1cd77019a3f787c691a8237331a8cc607139e6
Martin McCaffery <mccaffery.martin@gmail.com>
2020-05-26 12:32:48 +0100

    Added -a flag to docker image prune

--- image/docker-gc.sh
+++ image/docker-gc.sh
@@ -3,4 +3,4 @@
 docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro -e MINIMUM_IMAGES_TO_SAVE=3 -e FORCE_IMAGE_REMOVAL=1 spotify/docker-gc
 
 docker volume prune -f
-docker image prune -f
+docker image prune -af
