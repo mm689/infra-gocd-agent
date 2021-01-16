bd168c9142254bb214804a6a3af540c4efde34e2
Martin McCaffery <martin.mccaffery@thoughtworks.com>
2021-01-16 11:18:36 +0000

    Added docker volumes (back) to garbage collection

--- image/docker-gc.sh
+++ image/docker-gc.sh
@@ -2,4 +2,4 @@
 
 docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc:ro -e MINIMUM_IMAGES_TO_SAVE=3 -e FORCE_IMAGE_REMOVAL=1 spotify/docker-gc
 
-docker system prune -af
+docker system prune -af --volumes
